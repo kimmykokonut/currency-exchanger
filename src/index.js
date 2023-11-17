71ec2e6423b1cd28bb2f9195f5fe82cc21ec633e
Kim Robinson <kimmykokonut@gmail.com>
2023-11-17 10:49:16 -0800

    print conversion rate from usd to eur on dom

--- src/index.js
+++ src/index.js
@@ -3,19 +3,46 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import ExchangeService from './services/exchange-service.js';
 
-function handleTriangleForm(e) {
+async function getAPIData(selectCurrency) {
+  ExchangeService.getExchange(selectCurrency)
+    .then(function (exchangeResponse) {
+      if (exchangeResponse instanceof Error) {
+        const errorMessage = `There was a problem accessing the conversion exchange data from Exchange Rate API for ${selectCurrency}: ${exchangeResponse.message}`;
+        throw new Error(errorMessage);
+      }
+      // const codeTo = exchangeResponse.target_code; //eur
+      const convRate = exchangeResponse.conversion_rate; //0.9214
+      printRates(selectCurrency, convRate); //qty?
+    })
+    .catch(function (error) {
+      printError(error);
+    });
+}
+
+function printRates(selectCurrency, convRate) {
+  document.querySelector('#result').innerText = `The conversion rate for USD to ${selectCurrency} is ${convRate}. $qty here USD = ${(1 * convRate)} ${selectCurrency}.`;
+}
+
+function printError(error) {
+  document.querySelector('#error').innerText = error;
+}
+
+function clearResults() {
+  document.querySelector('#result').innerText = null;
+  document.querySelector('#error').innerText = null;
+}
+
+function handleConversionForm(e) {
   e.preventDefault();
-  document.querySelector('#response').innerText = null;
-  const length2 = parseInt(document.querySelector('#length2').value);
-  const length1 = parseInt(document.querySelector('#length1').value);
-  const length3 = parseInt(document.querySelector('#length3').value);
-  const triangle = new Triangle(length1, length2, length3);
-  const response = triangle.checkType();
-  const pTag = document.createElement("p");
-  pTag.append(response);
-  document.querySelector('#response').append(pTag);
+  clearResults();
+  // const howMuchMoney = document.querySelector('#qtyInput').value;
+  // document.querySelector('#qtyInput').value = null;
+  const selectCurrency = document.getElementById('currency').value;
+  console.log(selectCurrency);
+  getAPIData(selectCurrency);  //assume start usd at this point. maybe add second param later
+  //--------------------------
 }
 
 window.addEventListener("load", function () {
-  document.querySelector("#triangle-checker-form").addEventListener("submit", handleTriangleForm);
+  document.querySelector("#currency-exchange-form").addEventListener("submit", handleConversionForm);
 });
