e4e4aaba7ff615a1d559c9edc8d60efcf291b406
Kim Robinson <kimmykokonut@gmail.com>
2023-11-17 15:43:59 -0800

    add sessionStorage

--- src/services/exchange-service.js
+++ src/services/exchange-service.js
@@ -1,7 +1,13 @@
 export default class ExchangeService {
   static async getExchange(currencyFrom, currencyTo) {
+    const cacheKey = `${currencyFrom}_${currencyTo}`;
+    const cachedData = sessionStorage.getItem(cacheKey);
+    if (cachedData) {
+      return JSON.parse(cachedData);
+    }
     return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/${currencyFrom}/${currencyTo}`)
       .then(function (response) {
+        console.log(response);
         if (!response.ok) {
           return response.json()
             .then(function (apiErrorMsg) {
@@ -10,10 +16,13 @@ export default class ExchangeService {
               throw new Error(errorMessage);
             });
         } else {
-          return response.json();
+          const responseData = response.json();
+          sessionStorage.setItem(cacheKey, JSON.stringify(responseData));
+          return responseData;
         }
       })
       .catch(function (error) {
+        console.log(error);
         return error;
       });
   }
