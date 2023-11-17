504bee468528019bd8cbb902eee61f72c79e0be5
Kim Robinson <kimmykokonut@gmail.com>
2023-11-17 11:06:42 -0800

    add functionaly to convert inputted dollar amount

--- src/index.js
+++ src/index.js
@@ -3,24 +3,23 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import ExchangeService from './services/exchange-service.js';
 
-async function getAPIData(selectCurrency) {
+async function getAPIData(selectCurrency, howMuchMoney) {
   ExchangeService.getExchange(selectCurrency)
     .then(function (exchangeResponse) {
       if (exchangeResponse instanceof Error) {
         const errorMessage = `There was a problem accessing the conversion exchange data from Exchange Rate API for ${selectCurrency}: ${exchangeResponse.message}`;
         throw new Error(errorMessage);
       }
-      // const codeTo = exchangeResponse.target_code; //eur
-      const convRate = exchangeResponse.conversion_rate; //0.9214
-      printRates(selectCurrency, convRate); //qty?
+      const convRate = exchangeResponse.conversion_rate;
+      printRates(selectCurrency, convRate, howMuchMoney);
     })
     .catch(function (error) {
       printError(error);
     });
 }
 
-function printRates(selectCurrency, convRate) {
-  document.querySelector('#result').innerText = `The conversion rate for USD to ${selectCurrency} is ${convRate}. $qty here USD = ${(1 * convRate)} ${selectCurrency}.`;
+function printRates(selectCurrency, convRate, howMuchMoney) {
+  document.querySelector('#result').innerText = `The conversion rate for USD to ${selectCurrency} is ${convRate}. ${howMuchMoney} USD = ${(howMuchMoney * convRate)} ${selectCurrency}.`;
 }
 
 function printError(error) {
@@ -35,11 +34,10 @@ function clearResults() {
 function handleConversionForm(e) {
   e.preventDefault();
   clearResults();
-  // const howMuchMoney = document.querySelector('#qtyInput').value;
-  // document.querySelector('#qtyInput').value = null;
+  const howMuchMoney = document.querySelector('#qtyInput').value;
+  document.querySelector('#qtyInput').value = null;
   const selectCurrency = document.getElementById('currency').value;
-  console.log(selectCurrency);
-  getAPIData(selectCurrency);  //assume start usd at this point. maybe add second param later
+  getAPIData(selectCurrency, howMuchMoney);  //assume start usd at this point. maybe add second param later
   //--------------------------
 }
 
